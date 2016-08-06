75d532047b9d92fd921d8d8ee7ffdb99b4b43611
Manash Kumar Mandal <manashmndl@gmail.com>
2016-08-06 16:26:56 +0600

    added site files

---
+++ js/me.js
@@ -0,0 +1,13 @@
+// $(document).ready(function () {
+//     $('.nav li a').click(function(e) {
+//
+//         $('.nav li').removeClass('active');
+//
+//         var $parent = $(this).parent();
+//         if (!$parent.hasClass('active')) {
+//             $parent.addClass('active');
+//         }
+//         e.preventDefault();
+//     });
+// });
+
